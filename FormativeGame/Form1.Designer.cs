4e01314553adb3897ad3dbec9caeaf28e1f950fa
Anaami Patel <anaapate090@ed.amdsb.ca>
2020-02-11 12:32:38 -0500

    Add project files.

---
+++ FormativeGame/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace FormativeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.startButton.Location = new System.Drawing.Point(319, 159);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(176, 95);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Play Game";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(282, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 116);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FormativeGame/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace FormativeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            startButton.Text = "Play Game";
+
+            startButton.BackColor = Color.Black;
+            startButton.ForeColor = Color.White;
+
+            label1.Visible = false;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.alert);
+
+            label1.Visible = true;
+            startButton.Visible = false;
+
+            label1.Text = "Game will start in : 3";
+            alertPlayer.Play();
+            Refresh();
+            Thread.Sleep(2000);
+
+            label1.Text = "Game will start in : 2";
+            Refresh();
+            Thread.Sleep(2000);
+
+            label1.Text = "Game will start in : 1";
+            Refresh();
+            Thread.Sleep(2000);
+
+            this.BackColor = Color.Green;
+            label1.Text = "GO!!!";
+            alertPlayer.Play();
+            Refresh();
+            Thread.Sleep(2000);
+        }
+    }
+}
